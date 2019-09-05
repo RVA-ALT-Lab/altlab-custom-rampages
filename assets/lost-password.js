d5d46b3fe90631396e472db4351828300d26c295
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-09-05 15:54:32 -0400

    updates to add message to login screen and remove forgot password box

--- assets/lost-password.js
+++ assets/lost-password.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
   var lostPasswordLink = document.querySelectorAll("#login #nav a")[1];
-  lostPasswordLink.href = "/help/password-help/"
+
+  if (lostPasswordLink.innerText.includes('password')) {
+    lostPasswordLink.href = "/help/password-help/"
+  }
+
+  var loginBox = document.querySelector('#login');
+  var loginForm = document.querySelector('#login #loginform');
+  if (loginForm === null) {
+    loginForm = document.querySelector('#login #lostpasswordform')
+    if (loginForm !== null) {
+      loginForm.style.display = "none";
+    }
+  }
+
+  var message = document.createElement('p');
+  message.classList.add('message');
+  message.innerHTML = 'We are currently having an issue with most users not receiving password reset emails for @vcu.edu, @mymail.vcu.edu, and @vcuhealth.org email addresses. Please request <a href="/help/password-help/">a manual password reset using this form.</a>';
+  loginBox.insertBefore(message, loginForm);
+
 })
